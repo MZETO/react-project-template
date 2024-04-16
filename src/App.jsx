cf96a3361c418081900615e5caa7444432e1527e
MZETO <dani.martinez@tutanota.com>
2024-04-16 16:39:21 +0200

    feat: initial-commit with configs working

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './assets/styles/main.css';
+
+const App = () => {
+	return <div>react-project-template</div>;
+};
+
+export default App;
---
+++ src/App.test.jsx
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom';
+
+describe('App', () => {
+	test('testing tests', () => {
+		expect(true).toBe(true);
+	});
+});
